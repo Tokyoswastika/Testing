9450979cc9ecb386f94b56dc5565edb0de370ab8
DOVHAL KYRYLO <95588460+Tokyoswastika@users.noreply.github.com>
2024-10-01 10:44:54 +0300

    Add files via upload

---
+++ lab4/src/main/java/edu/cs/lab4crud/Lab4CrudApplication.java
@@ -0,0 +1,13 @@
+package edu.cs.lab4crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4CrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4CrudApplication.class, args);
+    }
+
+}
---
+++ lab4/src/main/java/edu/cs/lab4crud/controller/MealRestController.java
@@ -0,0 +1,51 @@
+package edu.cs.lab4crud.controller;
+
+/*
+  @author   DovhalKyrylo
+  @project   lab4
+  @class  MealRestController
+  @version  1.0.0
+  @since 1.10.2024 - 6.50
+*/
+
+import edu.cs.lab4crud.model.Meal;
+import edu.cs.lab4crud.repository.MealRepository;
+import edu.cs.lab4crud.service.MealService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/meals/")
+@RequiredArgsConstructor
+public class MealRestController {
+    private final MealService mealService;
+
+    @GetMapping
+    public List<Meal> getAll() {
+        return mealService.getAll();
+    }
+
+    // read one
+    @GetMapping("{id}")
+    public Meal showOneById(@PathVariable String id) {
+        return mealService.getById(id);
+    }
+
+    @PostMapping
+    public Meal insert(@RequestBody Meal Meal) {
+        return mealService.create(Meal);
+    }
+
+    @PutMapping
+    public Meal edit(@RequestBody Meal Meal) {
+        return mealService.update(Meal);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        mealService.delById(id);
+    }
+
+}
